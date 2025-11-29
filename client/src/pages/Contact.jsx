e005ba1df440a3737eee5bb2f3ca5a01f5547bc9
kamran alam <kammykamran0093@gmail.com>
2025-11-29 14:57:38 +0530

    Remove .env from tracking and add .gitignore

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,5 +1,63 @@
 export const Contact=()=>{
     return(
-        <h1>Contact</h1>
+        <section className="section-contact">
+            <div className="contact-content container">
+                <h1 className="main-heading">Contact Us</h1>
+            </div>
+
+            {/* contact page main */}
+            <div className="conatiner grid grid-two-cols">
+                <div className="contact-img">
+                    <img src="/images/support.png" alt="img" />
+                </div>
+
+                {/* actual content */}
+                <section className="section-form">
+                    <form onSubmit={handleSubmit} >
+                        <div>
+                            <label htmlFor="username">username</label>
+                            <input 
+                            type="text" 
+                            name="username" 
+                            id="username" 
+                            autoComplete="off"
+                            onChange={handleInput}
+                            value={contact.username}
+                            required
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="email">email</label>
+                            <input 
+                            type="text" 
+                            name="email" 
+                            id="email" 
+                            autoComplete="off"
+                            onChange={handleInput}
+                            value={contact.email}
+                            required
+                            />
+                        </div>
+
+                        <div>
+                            <label htmlFor="message">message</label>
+                            <textarea 
+                            name="message" 
+                            id="message"
+                            cols="30"
+                            rows="10"
+                            autoComplete="off"
+                            onChange={handleInput}
+                            value={contact.message}
+                            required
+                            ></textarea>
+                        </div>
+                        <div>
+                            <button type="submit">submit</button>
+                        </div>
+                    </form>
+                </section>
+            </div>
+        </section>
     )
 }
